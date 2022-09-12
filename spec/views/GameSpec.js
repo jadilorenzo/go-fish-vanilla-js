696b98c2b669123389d88d54d0859dbc58c72923
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2022-09-12 09:08:12 -0400

    Tests whole game

--- spec/views/GameSpec.js
+++ spec/views/GameSpec.js
@@ -32,4 +32,34 @@ describe('Game View', () => {
     expect(view.game().players()[1].hand().length).toBe(6)
     expect(view.game().currentPlayer().bot).toBe(undefined)
   })
+
+  it('plays round', () => {
+    view = new GameView({ game: new Game(), playerName: 'Player' })
+    view._game = new Game()
+    view._game.deal()
+    view.draw(container)
+
+    view.cardElement(0).click()
+    view.askButtonElement(1).click()
+    expect(view.game().players()[0].hand().length).toBe(8)
+    expect(view.game().players()[1].hand().length).toBe(6)
+    expect(view.game().currentPlayer().bot).toBe(undefined)
+  })
+
+  it('plays game', () => {
+    let res = false
+    view = new GameView({ game: new Game(), playerName: 'Player', callGameOver: () => { res = true } })
+    view._game = new Game()
+    view._game.deal()
+    view.draw(container)
+
+    while (!view.game().gameOver()) {
+      view.cardElement(0).click()
+      view.askButtonElement(1).click()
+    }
+    expect(view.game().players()[0].hand().length).toBe(0)
+    expect(view.game().players()[1].hand().length).toBe(0)
+    expect(view.game().currentPlayer().bot).toBe(undefined)
+    expect(res).toBe(true)
+  })
 })
